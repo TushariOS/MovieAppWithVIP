1177ec06ba2ba8259c43eaf13faf7359c281c304
JTushar <tushar@heady.io>
2019-01-16 11:48:20 +0530

    Initial Commit

---
+++ MovieBrwoserWithVIPER/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieBrwoserWithVIPER
+//
+//  Created by Tushar on 16/01/19.
+//  Copyright © 2019 Tushar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
